0b7c729613a7a4eb1e769e24aec485e133113bfd
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2022-12-27 00:03:02 -0300

    feat: create signup controller with validations fields

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,11 @@
+import { HttpResponse } from 'presentation/protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
