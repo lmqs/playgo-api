71157fdc3ba0c4c73e2e16ed89b762cf86d8e09a
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-04 15:24:33 -0300

    feat: include unautorhized error

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,9 +1,13 @@
-import { ServerError } from '../../presentation/errors'
+import { ServerError, UnauthorizedError } from '../errors'
 import { HttpResponse } from '../../presentation/protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
 })
 
 export const forbidden = (error: Error): HttpResponse => ({
