4f9e3e0486c13a0e50e3419f6717afc5dfb0a539
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-11 14:59:12 -0300

    test: create loadByUser and updateAccessToken methods in database folder

--- src/infra/database/postgres/account-repository/account.spec.ts
+++ src/infra/database/postgres/account-repository/account.spec.ts
@@ -26,7 +26,7 @@ describe('Account Postgres Repository', () => {
     }
   })
 
-  it('Sould return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const { sut } = makeSut()
 
     const accountDataMock = { id: 'valid_id', ...accountDataGenericMock }
@@ -43,5 +43,31 @@ describe('Account Postgres Repository', () => {
       cityId: 1,
       phoneNumber: 'valid_number'
     })
+  })
+
+  test('Should return an account on add success', async () => {
+    const { sut } = makeSut()
+    const accountDataMock = { id: 'valid_id', ...accountDataGenericMock }
+    sut.findOne = jest.fn().mockReturnValue(accountDataMock)
+
+    const account = await sut.loadByUser('valid_user')
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      user: 'valid_user',
+      password: 'valid_password',
+      email: 'valid_email',
+      cityId: 1,
+      phoneNumber: 'valid_number'
+    })
+  })
+
+  test('Should return undefined if loadByUser fails', async () => {
+    const { sut } = makeSut()
+    sut.findOne = jest.fn().mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.loadByUser('valid_user')
+    await expect(promise).rejects.toThrow()
   })
 })
