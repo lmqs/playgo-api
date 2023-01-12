94add89c2ed6da566fd1fd881adcab9772a84469
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-12 14:36:47 -0300

    refactor: separete factories in folders

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -2,20 +2,11 @@ import { LogControllerDecorator } from '../../decorators/log-controller-decorato
 import { Controller } from '../../../presentation/protocols'
 import { LogPostgresRepository } from '../../../infra/database/postgres/log/log-repository'
 import { LoginController } from '../../../presentation/controllers/login/login-controller'
-import { DbAuthentication } from '../../../data/usescases/authentication/db-authentication'
 import { makeLoginValidation } from './login-validation-factory'
-import { AccountPostgresRepository } from '../../../infra/database/postgres/account/account-repository'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
-import { ENVIRONMENT } from '../../config/env'
+import { makeDbAuthentication } from '../usecases/authentication/db-authentication-factory'
 
 export const makeLoginController = (): Controller => {
-  const accountRepository = new AccountPostgresRepository()
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const jwtAdapter = new JwtAdapter(ENVIRONMENT.server.secret)
-  const dbAuthentication = new AccountPostgresRepository()
-  const authentication = new DbAuthentication(accountRepository, bcryptAdapter, jwtAdapter, dbAuthentication)
+  const authentication = makeDbAuthentication()
   const loginController = new LoginController(authentication, makeLoginValidation())
   const logPostgresRepository = new LogPostgresRepository()
   return new LogControllerDecorator(loginController, logPostgresRepository)
---
+++ src/main/factories/usecases/add-account/db-add-account.ts
@@ -0,0 +1,11 @@
+import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { DbAddAccount } from '../../../../data/usescases/add-account/db-add-account'
+import { AccountPostgresRepository } from '../../../../infra/database/postgres/account/account-repository'
+import { AddAccount } from '../../../../domain/usecases/add-account'
+
+export const makeDbAddAccount = (): AddAccount => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountPostgresRepository = new AccountPostgresRepository()
+  return new DbAddAccount(bcryptAdapter, accountPostgresRepository)
+}
