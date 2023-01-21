2796f5aee425dc0dbb61721a1cbb024c62175bae
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-21 11:21:09 -0300

    test: implemented loadByToken in account postgres repository

--- src/infra/database/postgres/account/account-repository.spec.ts
+++ src/infra/database/postgres/account/account-repository.spec.ts
@@ -25,49 +25,96 @@ describe('Account Postgres Repository', () => {
       phoneNumber: 'valid_number'
     }
   })
+  describe('add()', () => {
+    test('Should return an account on add success', async () => {
+      const { sut } = makeSut()
 
-  test('Should return an account on add success', async () => {
-    const { sut } = makeSut()
+      const accountDataMock = { id: 'valid_id', ...accountDataGenericMock }
+      sut.create = jest.fn().mockReturnValue(accountDataMock)
 
-    const accountDataMock = { id: 'valid_id', ...accountDataGenericMock }
-    sut.create = jest.fn().mockReturnValue(accountDataMock)
+      const account = await sut.add(accountDataGenericMock)
 
-    const account = await sut.add(accountDataGenericMock)
-
-    expect(account).toEqual({
-      id: 'valid_id',
-      name: 'valid_name',
-      user: 'valid_user',
-      password: 'valid_password',
-      email: 'valid_email',
-      cityId: 1,
-      phoneNumber: 'valid_number'
+      expect(account).toEqual({
+        id: 'valid_id',
+        name: 'valid_name',
+        user: 'valid_user',
+        password: 'valid_password',
+        email: 'valid_email',
+        cityId: 1,
+        phoneNumber: 'valid_number'
+      })
     })
   })
 
-  test('Should return an account on add success', async () => {
-    const { sut } = makeSut()
-    const accountDataMock = { id: 'valid_id', ...accountDataGenericMock }
-    sut.findOne = jest.fn().mockReturnValue(accountDataMock)
+  describe('loadByUser()', () => {
+    test('Should return an account on add success', async () => {
+      const { sut } = makeSut()
+      const accountDataMock = { id: 'valid_id', ...accountDataGenericMock }
+      sut.findOne = jest.fn().mockReturnValue(accountDataMock)
 
-    const account = await sut.loadByUser('valid_user')
-    expect(account).toEqual({
-      id: 'valid_id',
-      name: 'valid_name',
-      user: 'valid_user',
-      password: 'valid_password',
-      email: 'valid_email',
-      cityId: 1,
-      phoneNumber: 'valid_number'
+      const account = await sut.loadByUser('valid_user')
+      expect(account).toEqual({
+        id: 'valid_id',
+        name: 'valid_name',
+        user: 'valid_user',
+        password: 'valid_password',
+        email: 'valid_email',
+        cityId: 1,
+        phoneNumber: 'valid_number'
+      })
+    })
+
+    test('Should return undefined if loadByUser fails', async () => {
+      const { sut } = makeSut()
+      sut.findOne = jest.fn().mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.loadByUser('valid_user')
+      await expect(promise).rejects.toThrow()
     })
   })
 
-  test('Should return undefined if loadByUser fails', async () => {
-    const { sut } = makeSut()
-    sut.findOne = jest.fn().mockImplementationOnce(() => {
-      throw new Error()
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken success whithout role', async () => {
+      const { sut } = makeSut()
+      const accountDataMock = [{ id: 'valid_id', ...accountDataGenericMock }]
+      sut.findGeneric = jest.fn().mockReturnValue(accountDataMock)
+
+      const account = await sut.loadByToken('any_token')
+      expect(account).toEqual({
+        id: 'valid_id',
+        name: 'valid_name',
+        user: 'valid_user',
+        password: 'valid_password',
+        email: 'valid_email',
+        cityId: 1,
+        phoneNumber: 'valid_number'
+      })
     })
-    const promise = sut.loadByUser('valid_user')
-    await expect(promise).rejects.toThrow()
+    test('Should return an account on loadByToken success with role', async () => {
+      const { sut } = makeSut()
+      const accountDataMock = [{ id: 'valid_id', ...accountDataGenericMock }]
+      sut.findGeneric = jest.fn().mockReturnValue(accountDataMock)
+
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toEqual({
+        id: 'valid_id',
+        name: 'valid_name',
+        user: 'valid_user',
+        password: 'valid_password',
+        email: 'valid_email',
+        cityId: 1,
+        phoneNumber: 'valid_number'
+      })
+    })
+
+    test('Should return undefined if loadByToken fails', async () => {
+      const { sut } = makeSut()
+      sut.findGeneric = jest.fn().mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.loadByToken('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
