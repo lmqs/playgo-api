8673f0ac522a2577ed889ec0b90214280dc0a01c
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-21 10:42:41 -0300

    feat: add decryp method in JWTAdapter

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,20 @@
+import { Decrypter } from '../../../data/usescases/protocols/criptography/decrypter'
 import jwt from 'jsonwebtoken'
 import { Encrypter } from '../../../data/usescases/protocols/criptography'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret)
     return accessToken
+  }
+
+  async decrypt (token: string): Promise<string | undefined> {
+    const value: any = await jwt.verify(token, this.secret)
+    if (value) {
+      return value
+    }
+    return undefined
   }
 }
