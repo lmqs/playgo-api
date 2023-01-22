2f1b174583e67681a2969a0f36b4f0c7fae7c3ca
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-22 10:30:01 -0300

    feat: create adapter middleware

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import { Middleware } from '../../presentation/protocols'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const request = {
+      headers: req.headers
+    }
+    const httpResponse = await middleware.handle(request)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
