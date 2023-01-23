f5660ce0d2933383ced6a53c97783c8188b6e012
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-22 21:40:20 -0300

    refactor: change params in loadByToken function

--- src/data/usescases/account/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usescases/account/load-account-by-token/db-load-account-by-token.ts
@@ -12,11 +12,10 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   async load (accessToken: string, role?: string): Promise<AccountModel | undefined> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      const account = await this.loadAccountByTokenRepository.loadByToken(token, role)
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       if (account) {
         return account
       }
     }
-    return undefined
   }
 }
