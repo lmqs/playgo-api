98f16e53e091f76146c7a0a2638ad8167f953854
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-22 21:02:41 -0300

    feat: create add-tournament domain use case

---
+++ src/domain/usecases/add-tournament.ts
@@ -0,0 +1,16 @@
+import { TournamentModel } from '../../domain/models/tournament'
+
+export interface AddTournamentModel {
+  description: string
+  cityId: string
+  sportId: string
+  dtTournament: string
+  registrationLimit: string
+  registrationStartDate: string
+  registrationFinalDate: string
+  deleted?: boolean
+}
+
+export interface AddTournament {
+  add: (tournament: AddTournamentModel) => Promise<TournamentModel | undefined>
+}
