f38669ebfc98dde5a1d1f2f36810a99f59146d7d
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-24 14:19:08 -0300

    test: add loadByTournamentId in infra/database

--- src/infra/database/postgres/category/category-repository.spec.ts
+++ src/infra/database/postgres/category/category-repository.spec.ts
@@ -1,3 +1,4 @@
+import { CategoryModel } from 'domain/models/category'
 import { AddCategoryModel } from '../../../../domain/usecases/add-category'
 import { CategoryPostgresRepository } from './category-repository'
 
@@ -18,20 +19,69 @@ const makeFakeAddCategoryModel = (): AddCategoryModel => ({
   numberAthletes: 'valid_numberAthletes'
 })
 
+const makeFakeCategoryModel = (): CategoryModel => ({
+  id: 'valid_id',
+  description: 'valid_description',
+  tournamentId: 'valid_tournamentId',
+  numberAthletes: 'valid_numberAthletes'
+})
+
 describe('Category Postgres Repository', () => {
-  test('Should return an Category on add success', async () => {
-    const { sut } = makeSut()
+  describe('add()', () => {
+    test('Should return an Category on add success', async () => {
+      const { sut } = makeSut()
+      sut.create = jest.fn().mockReturnValue(makeFakeCategoryModel())
 
-    const categoryDataMock = { id: 'valid_id', ...makeFakeAddCategoryModel() }
-    sut.create = jest.fn().mockReturnValue(categoryDataMock)
+      const category = await sut.add(makeFakeAddCategoryModel())
 
-    const category = await sut.add(makeFakeAddCategoryModel())
+      expect(category).toEqual({
+        id: 'valid_id',
+        description: 'valid_description',
+        tournamentId: 'valid_tournamentId',
+        numberAthletes: 'valid_numberAthletes'
+      })
+    })
 
-    expect(category).toEqual({
-      id: 'valid_id',
-      description: 'valid_description',
-      tournamentId: 'valid_tournamentId',
-      numberAthletes: 'valid_numberAthletes'
+    test('Should return undefined if create fails', async () => {
+      const { sut } = makeSut()
+      sut.create = jest.fn().mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.add(makeFakeAddCategoryModel())
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('loadByTournamentId()', () => {
+    test('Should return an categories list on loadByTournamentId success', async () => {
+      const { sut } = makeSut()
+      sut.findGeneric = jest.fn().mockReturnValue([makeFakeCategoryModel(), makeFakeCategoryModel()])
+
+      const category = await sut.loadByTournamentId('valid_tournamentId')
+      expect(category?.length).toBe(2)
+      expect(category).toEqual([
+        {
+          id: 'valid_id',
+          description: 'valid_description',
+          tournamentId: 'valid_tournamentId',
+          numberAthletes: 'valid_numberAthletes'
+        },
+        {
+          id: 'valid_id',
+          description: 'valid_description',
+          tournamentId: 'valid_tournamentId',
+          numberAthletes: 'valid_numberAthletes'
+        }
+      ])
+    })
+
+    test('Should return undefined if loadByTournamentId fails', async () => {
+      const { sut } = makeSut()
+      sut.findGeneric = jest.fn().mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.loadByTournamentId('valid_tournamentId')
+      await expect(promise).rejects.toThrow()
     })
   })
 })
