c49cde8a39d269ae5354fb4fd741e165f600539b
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-24 14:38:01 -0300

    feat: added route and factories to loadCategoriesByTournamentId

---
+++ src/main/factories/controllers/load-category/load-categories-by-tournamentId-controller-factory.ts
@@ -0,0 +1,12 @@
+import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
+import { Controller } from '../../../../presentation/protocols'
+import { makeLoadCategoriesByTournamentIdValidation } from './load-categories-by-tournamentId-validation-factory'
+import { LoadCategoriesByTournamentIdController } from 'presentation/controllers/category/load-category-by-tournamentId/load-category-by-tournamentId-controller'
+import { makeDbLoadCategories } from '../../usecases/db-load-categories-by-tournamentId/db-load-categories-by-tournamentId'
+import { LogPostgresRepository } from '../../../../infra/database/postgres/log/log-repository'
+
+export const makeLoadCategoriesByTournamentIdController = (): Controller => {
+  const addCategoryController = new LoadCategoriesByTournamentIdController(makeLoadCategoriesByTournamentIdValidation(), makeDbLoadCategories())
+  const logPostgresRepository = new LogPostgresRepository()
+  return new LogControllerDecorator(addCategoryController, logPostgresRepository)
+}
--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -3,8 +3,11 @@ import { adaptMiddleware } from '../../main/adapters/express-middleware-adapter'
 import { makeAuthMiddleware } from '../../main/factories/middleware/auth-middleware-factory'
 import { makeAddCategoryController } from '../../main/factories/controllers/add-category-controller-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeLoadCategoriesByTournamentIdController } from '../../main/factories/controllers/load-category/load-categories-by-tournamentId-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware(''))
   router.post('/category', adminAuth, adaptRoute(makeAddCategoryController()))
+  router.post('/loadCategoriesByTournamentId', auth, adaptRoute(makeLoadCategoriesByTournamentIdController()))
 }
