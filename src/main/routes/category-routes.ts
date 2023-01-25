2e21a41a72d1ed3c0c83623ac45ce3546db843b6
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-24 23:18:35 -0300

    refactor: move auth middlewares to separate files

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
-import { adaptMiddleware } from '../../main/adapters/express-middleware-adapter'
-import { makeAuthMiddleware } from '../../main/factories/middleware/auth-middleware-factory'
 import { makeAddCategoryController } from '../factories/controllers/add-category/add-category-controller-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeLoadCategoriesByTournamentIdController } from '../../main/factories/controllers/load-category/load-categories-by-tournamentId-controller-factory'
+import { makeLoadCategoriesByTournamentIdController } from '../factories/controllers/load-category/load-categories-by-tournamentId-controller-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware(''))
   router.post('/category', adminAuth, adaptRoute(makeAddCategoryController()))
   router.post('/loadCategoriesByTournamentId', auth, adaptRoute(makeLoadCategoriesByTournamentIdController()))
 }
