3a5ad1ebc8f227937552e57d8caff73897c264c6
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-25 00:17:10 -0300

    refactor: correct import

--- src/main/factories/controllers/load-category/load-categories-by-tournamentId-controller-factory.ts
+++ src/main/factories/controllers/load-category/load-categories-by-tournamentId-controller-factory.ts
@@ -1,7 +1,7 @@
 import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
 import { Controller } from '../../../../presentation/protocols'
 import { makeLoadCategoriesByTournamentIdValidation } from './load-categories-by-tournamentId-validation-factory'
-import { LoadCategoriesByTournamentIdController } from 'presentation/controllers/category/load-category-by-tournamentId/load-category-by-tournamentId-controller'
+import { LoadCategoriesByTournamentIdController } from '../../../../presentation/controllers/category/load-category-by-tournamentId/load-category-by-tournamentId-controller'
 import { makeDbLoadCategories } from '../../usecases/db-load-categories-by-tournamentId/db-load-categories-by-tournamentId'
 import { LogPostgresRepository } from '../../../../infra/database/postgres/log/log-repository'
 
