f09f7d1bf9aefcdde3913bbac1f8e575cfa7e7a3
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-26 13:29:43 -0300

    test: factory add tournament validation

---
+++ src/main/factories/controllers/tournament/add-tournament/add-tournament-validation-factory.spec.ts
@@ -0,0 +1,18 @@
+import { Validation } from '../../../../../presentation/protocols/validation'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../../validation/validators'
+import { makeAddTournamentValidation } from './add-tournament-validation-factory'
+
+jest.mock('../../../../../validation/validators/validation-composite')
+
+describe('makeAddCategoryValidation ', () => {
+  test('Sould call ValidationComposite with all validations', () => {
+    makeAddTournamentValidation()
+    const validations: Validation[] = []
+    const requiredFields = ['description', 'cityId', 'sportId', 'dtTournament', 'registrationStartDate', 'registrationFinalDate']
+
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
