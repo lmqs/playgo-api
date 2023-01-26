e95e1ef10bc2bf6c8dbb769c7d0a7c24ffd5ed6d
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-26 16:07:16 -0300

    refactor: change url import

--- src/data/protocols/db/category/add-category-repository.ts
+++ src/data/protocols/db/category/add-category-repository.ts
@@ -1,5 +1,5 @@
 import { CategoryModel } from '../../../../domain/models/category'
-import { AddCategoryModel } from '../../../../domain/usecases/add-category'
+import { AddCategoryModel } from '../../../../domain/usecases/category/add-category'
 
 export interface AddCategoryRepository {
   add: (category: AddCategoryModel) => Promise<CategoryModel | undefined >
