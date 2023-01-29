6294314a823cbe1dfac6d290f9d54ffc25878e10
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-28 23:12:00 -0300

    refactor: change configs folder

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { PostgresService } from '@/infra/database/postgres/postgres-service'
 
 PostgresService.getInstance().connect().then(
   async () => {
-    const { setupApp } = await import('@/main/express/app')
+    const { setupApp } = await import('@/main/config/app')
     const { ENVIRONMENT } = await import ('@/main/config')
     const app = await setupApp()
     app.listen(ENVIRONMENT.server.port, () => {
