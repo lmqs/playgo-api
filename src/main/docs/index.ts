ca5f93f9224ff57d3689a25ea3c6157d62f593f7
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-01-29 22:05:55 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,30 @@
+import { loginPath } from '@/main/docs/paths/login-path'
+import { accountSchema } from '@/main/docs/schemas/account-schemas'
+import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: '',
     description: 'API do app PlayGo',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [
+    {
+      name: 'Login'
+    },
+    {
+      name: 'Category'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
