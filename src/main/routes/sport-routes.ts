2fc8c5bde9b5335b2db65821e9057605874a4d0f
LucianaQueiroz <lucianaqueiroz098@hotmail.com>
2023-03-20 22:52:15 -0300

    fix: alter role change in get tournmant all and sport all

--- src/main/routes/sport-routes.ts
+++ src/main/routes/sport-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { adminAuth } from '@/main/middlewares/admin-auth'
-import { makeAddSportController } from '../factories/controllers/sport/add-sport/add-sport-controller-factory'
-import { makeLoadSportsController } from '../factories/controllers/sport/load-sport/load-sports-controller-factory'
+import { makeAddSportController } from '@/main/factories/controllers/sport/add-sport/add-sport-controller-factory'
+import { makeLoadSportsController } from '@/main/factories/controllers/sport/load-sport/load-sports-controller-factory'
+import { auth } from '@/main/middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/sport', adminAuth, adaptRoute(makeAddSportController()))
-  router.get('/sports', adminAuth, adaptRoute(makeLoadSportsController()))
+  router.get('/sports', auth, adaptRoute(makeLoadSportsController()))
 }
--- src/main/routes/tournament-routes.ts
+++ src/main/routes/tournament-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/express-route-adapter'
-import { adminAuth } from '../middlewares/admin-auth'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { adminAuth } from '@/main/middlewares/admin-auth'
+import { auth } from '@/main/middlewares/auth'
 import { makeRemoveTournamentsController, makeLoadTournamentsController, makeAddTournamentController } from '@/main/factories/controllers/tournament'
 
 export default (router: Router): void => {
   router.post('/tournament', adminAuth, adaptRoute(makeAddTournamentController()))
-  router.get('/tournaments', adminAuth, adaptRoute(makeLoadTournamentsController()))
+  router.get('/tournaments', auth, adaptRoute(makeLoadTournamentsController()))
   router.delete('/tournament/:id', adminAuth, adaptRoute(makeRemoveTournamentsController()))
 }
